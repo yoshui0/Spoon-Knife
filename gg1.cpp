723bd91f249ffa0b8a038c8a8d558e76ea8437c4
yoshui0 <125722749+yoshui0@users.noreply.github.com>
2023-02-18 12:26:43 +0530

    Update gg1.cpp

--- gg1.cpp
+++ gg1.cpp
@@ -1,14 +1,8 @@
+// Your First C++ Program
+
 #include <iostream>
-#include <fstream>
-using namespace std;
 
 int main() {
-  // Create and open a text file
-  ofstream MyFile("filename.txt");
-
-  // Write to the file
-  MyFile << "Files can be tricky, but it is fun enough!";
-
-  // Close the file
-  MyFile.close();
-}
+    std::cout << "Hello World!";
+    return 0;
+}s
